46636551d701fab35433d41a6637bb4c35a0b3f8
brunsa2 <brunsa2@gmail.com>
2010-06-24 22:07:43 -0500

    created database access class

---
+++ src/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+$databaseConfiguration['host'] = 'localhost';
+$databaseConfiguration['user'] = 'default';
+$databaseConfiguration['password'] = 'default';
+$databaseConfiguration['database'] = 'contenttree';
+
+?>
