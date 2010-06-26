2bb54023a99a22e5c169ef211975dbaf853b3105
brunsa2 <brunsa2@gmail.com>
2010-06-26 15:16:25 -0500

    modified add and update methods to use timestamps and avoid conflict and make seperate branches for the same content introduced at different times

--- src/ContentTree.php
+++ src/ContentTree.php
@@ -12,22 +12,28 @@ class ContentTree {
 	}
 	
 	public function addArticle($contents = '') {
-		$blankBlobId = sha1('');
-		$newBlobId = sha1($contents);
-		$newArticleId = sha1($blankBlobId . $contents);
+		$timestamp = date('Y-m-d H:i:s');
 		
-		$this->database->query("insert into blobs values('$newBlobId', '$blankBlobId', null, true, false, false, false, 0, 1, '$contents');");
-		$this->database->query("insert into articles values('$newArticleId', '$newBlobId', '$newBlobId');");
+		$blankBlobId = sha1('');
+		$newBlobId = sha1($contents . $timestamp);
+		$newArticleId = sha1($blankBlobId . $contents . $timestamp);
+		
+		$this->database->query("insert into blobs values('$newBlobId', '$blankBlobId', null, true, false, false, false, '$timestamp', 0, 1, '$contents');");
+		$this->database->query("insert into articles values('$newArticleId', '$timestamp', '$newBlobId', '$newBlobId');");
 	}
 	
 	public function updateArticle($parentId, $contents = '') {
-		$newBlobId = sha1($contents);
-		$newArticleId = sha1($parentId . $contents);
+		$timestamp = date('Y-m-d H:i:s');
 		
-		$this->database->query("insert into blobs values('$newBlobId', '$parentId', null, true, false, false, false, 0, 1, '$contents');");
-		$this->database->query("insert into articles values('$newArticleId', '$newBlobId', '$newBlobId');");
+		$newBlobId = sha1($contents . $timestamp);
+		$newArticleId = sha1($parentId . $contents . $timestamp);
+		
+		$this->database->query("insert into blobs values('$newBlobId', '$parentId', null, true, false, false, false, '$timestamp', 0, 1, '$contents');");
+		$this->database->query("insert into articles values('$newArticleId', '$timestamp', '$newBlobId', '$newBlobId');");
 		
 		$this->database->query("update blobs set tip = false, content_references = content_references + 1 where sha = '$parentId';");
 	}
+	
+	
 	
 }
